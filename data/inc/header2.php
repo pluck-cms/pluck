3767bf72b88ac8713bef97cefea43a84e7cbe934
Sander Thijsen <troep@somp.nl>
2008-08-05 19:57:27 +0200

    Deny direct access to edited files.

--- data/inc/header2.php
+++ data/inc/header2.php
@@ -11,6 +11,14 @@
  * See docs/COPYING for the complete license.
 */
 
+//Make sure the file isn't accessed directly
+if((!ereg("index.php", $_SERVER['SCRIPT_FILENAME'])) && (!ereg("admin.php", $_SERVER['SCRIPT_FILENAME'])) && (!ereg("install.php", $_SERVER['SCRIPT_FILENAME'])) && (!ereg("login.php", $_SERVER['SCRIPT_FILENAME']))){
+    //Give out an "access denied" error
+    echo "access denied";
+    //Block all other code
+    exit();
+}
+
 //We need tinyMCE...
 $tinymce = "yes";
 
