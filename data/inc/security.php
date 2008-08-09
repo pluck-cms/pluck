b113a5079f5f53433660f527e94846ed0ca3b66f
Sander Thijsen <troep@somp.nl>
2008-08-09 13:17:30 +0200

    Fix bug #256182

--- data/inc/security.php
+++ data/inc/security.php
@@ -33,8 +33,7 @@ if(isset($_REQUEST)) {
 //Check for strange characters in $_GET keys
 //All keys with or "/" or ".." or ":" or "<" or ">" or "=" or ";" or ")" are blocked, so that it's virtually impossible to inject any HTML-code, or external websites
 foreach ($_GET as $get_key => $get_value) {
-	if ((ereg("[\]+", $get_value)) || (ereg("..", $get_value)) || (ereg("/", $get_value)) || (ereg(":", $get_value)) || (ereg("<", $get_value)) || (ereg(">", $get_value)) || (ereg("=", $get_value)) || (ereg(";", $get_value)) || (ereg(")", $get_value))) {
-		eval("unset(\${$get_key});");
+	if ((ereg("[\]+", $get_value)) || (ereg("\.\.", $get_value)) || (ereg("/", $get_value)) || (ereg(":", $get_value)) || (ereg("<", $get_value)) || (ereg(">", $get_value)) || (ereg("=", $get_value)) || (ereg(";", $get_value)) || (ereg(")", $get_value))) {
 		die("A hacking attempt has been detected. For security reasons, we're blocking any code execution.");
 	}
 }
