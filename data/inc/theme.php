e53ce195678cb5997fd4a9457ec1e6e052ce6905
Sander Thijsen <troep@somp.nl>
2008-08-16 18:12:45 +0200

    Make sure currently used theme is selected + code cleanup

--- data/inc/theme.php
+++ data/inc/theme.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/*
  * This file is part of pluck, the easy content management system
  * Copyright (c) somp (www.somp.nl)
  * http://www.pluck-cms.org
@@ -7,7 +7,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- 
+
  * See docs/COPYING for the complete license.
 */
 
@@ -18,39 +18,43 @@ if((!ereg("index.php", $_SERVER['SCRIPT_FILENAME'])) && (!ereg("admin.php", $_SE
     //Block all other code
     exit();
 }
+?>
 
-//Introduction text
-echo "<div class=\"rightmenu\">
-<div style=\"background-color: #f4f4f4; padding: 5px; width: 300px; margin-top: 15px; border: 1px dotted gray;\">
+<div class="rightmenu">
+<div style="background-color: #f4f4f4; padding: 5px; width: 300px; margin-top: 15px; border: 1px dotted gray;">
 	<table>
 		<tr>
 			<td>
-				<img src=\"data/image/install.png\" border=\"0\" alt=\"\">
+				<img src="data/image/install.png" border="0" alt="">
 			</td>
-			<td>";
+			<td>
+			<?php
 				// if zlib is installed
 				if (get_extension_funcs('zlib')) {
-				echo "<span style=\"font-size: 17pt;\"><a href=\"?action=themeinstall\">$lang_theme5</a></span>";
+					echo '<span style="font-size: 17pt;"><a href="?action=themeinstall">'.$lang_theme5.'</a></span>';
 				}
 				// if zlib is not installed				
 				elseif (!get_extension_funcs('zlib')) {
-				echo "<span class=\"kop3\">$lang_theme5</span><br>
-				<span style=\"color: red;\">$lang_theme14</span>";
+					echo '<span class="kop3">'.$lang_theme5.'</span><br>';
+					echo '<span style="color: red;">'.$lang_theme14.'</span>';
 				} 
-			echo "</td>
+			?></td>
 		</tr>
 	</table>
 </div>
 </div>
-<p><b>$lang_theme1</b></p>
+<p><b><?php echo $lang_theme1; ?></b></p>
 
+<form action="" method="post">
+	<select name="cont">
+		<option value="0"><?php echo $lang_lang2; ?>
 
-<form action=\"\" method=\"post\">
-<select name=\"cont\">
-<option selected value=\"0\">$lang_lang2";
-
+<?php
 //Function to read out the themes
 function read_dir($dir) {
+	//Include theme information
+	include('data/settings/themepref.php');
+
    $path = opendir($dir);
    while (false !== ($file = readdir($path))) {
        if(($file !== ".") and ($file !== "..") and ($file !== "themepref.php") and ($file !== "predefined_variables.php")) {
@@ -65,7 +69,14 @@ function read_dir($dir) {
        foreach ($dirs as $dir) {
 			if (file_exists("$dir/info.php")) {
 				include ("$dir/info.php");
-				echo "<option value=\"$themedir\">$themename";
+				//If theme is current theme, select it
+				if($themepref == $themedir) {
+					echo '<option value="'.$themedir.'" selected>'.$themename."\n";
+				}
+				//Otherwise, don't select it
+				else {
+					echo '<option value="'.$themedir.'">'.$themename."\n";
+				}
 			}
        }
    }
@@ -77,11 +88,14 @@ function read_dir($dir) {
 
 //Actually read out the dir
 read_dir("data/themes"); 
-echo "</select><br>
-<input type=\"submit\" name=\"Submit\" value=\"$lang_install13\">
-<input type=\"button\" name=\"Cancel\" value=\"$lang_install14\" onclick=\"javascript: window.location='?action=options';\">
-</form>";
- 
+?>
+
+	</select><br />
+	<input type="submit" name="Submit" value="<?php echo $lang_install13; ?>">
+	<input type="button" name="Cancel" value="<?php echo $lang_install14; ?>" onclick="javascript: window.location='?action=options';">
+</form>
+
+<?php
 //Save the theme-data
 if((isset($_POST['Submit'])) && ($cont != "0") && (file_exists("data/themes/$cont"))) {
 	$data = "data/settings/themepref.php";    
@@ -89,7 +103,7 @@ if((isset($_POST['Submit'])) && ($cont != "0") && (file_exists("data/themes/$con
 	fputs($file, "<?php \$themepref = \"$cont\"; ?>");  
 	fclose($file);
 	chmod($data,0777);
-	
+
 	//Redirect user
 	echo $lang_theme3;
 	redirect("?action=options","0");
