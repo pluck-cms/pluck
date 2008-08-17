759d1257475da11ef059093a6fc60c0a68c142c5
Sander Thijsen <troep@somp.nl>
2008-08-17 02:27:18 +0200

    Add Anders to credits

--- data/inc/credits.php
+++ data/inc/credits.php
@@ -36,6 +36,7 @@ if((!ereg("index.php", $_SERVER['SCRIPT_FILENAME'])) && (!ereg("admin.php", $_SE
 
 <p>
 	<span class="kop2"><?php echo $lang_credits7; ?></span><br />
+	Anders Jørgensen<br />
 	Kristaps Ancāns<br />
 	Dennis Sewbarath
 </p>
