29e67934c526000c5d025284691720078e4f9fc2
Sander Thijsen <troep@somp.nl>
2008-09-20 14:26:20 +0200

    Some first code for posting and displaying reactions

--- data/modules/blog/pages_site/viewpost.php
+++ data/modules/blog/pages_site/viewpost.php
@@ -26,9 +26,14 @@ $pageback = $_GET['pageback'];
 //First, check if the page exists
 if(file_exists('data/settings/modules/blog/posts/'.$post)) {
 	include('data/settings/modules/blog/posts/'.$post);
+}
+else {
+	redirect('?file=kop1.php','0');
+	exit;
+}
 ?>
 
-<div class="blogpost" style="margin-top: 20px">
+<div class="blogpost" style="margin-top: 20px;">
 	<span class="posttitle" style="font-size: 18px;">
 		<?php echo $post_title; ?>
 	</span><br />
@@ -38,132 +43,127 @@ if(file_exists('data/settings/modules/blog/posts/'.$post)) {
 	<?php echo $post_content; ?>
 </div>
 
+<div style="margin-top: 10px;">
+	<span style="font-size: 19px"><?php echo $lang_blog16; ?></span>
+
 <?php
-}
-echo "<div style=\"margin-bottom: 20px;\">
-<span class=\"postinfo\" style=\"font-size: 10px;\">$lang_blog14 <span style=\"font-weight: bold;\">$cat</span> - $postdate</span><br /><br />
-$content
-</div>";
+//Then show the reactions
+//Check if there are reactions
+if(isset($post_reaction_title)) {
+	foreach($post_reaction_title as $key => $value) { ?>
+<div class="blogpost" style="margin-bottom: 15px; margin-top: 5px;">
+	<span class="posttitle" style="font-size: 16px;">
+		<?php echo $post_reaction_title[$key]; ?>
+	</span><br />
 
-//Then show form the reactions
-echo "<span style=\"font-size: 19px\">$lang_blog16</span>";
-
-//Define the function to readout the reactions
-function read_reactions($dir) {
-   $path = opendir($dir);
-   while (false !== ($file = readdir($path))) {
-       if(($file !== ".") and ($file !== "..")) {
-           if(is_file($dir."/".$file))
-               $files[]=$file;
-           else
-               $dirs[]=$dir."/".$file;    
-       }
-   }
-   if($dirs) {
-   echo "";
-   }
-   if($files) {
-       natcasesort($files);
-		 $files = array_reverse($files);
-       foreach ($files as $file) {
-			  $blogpost = $_GET['blogpost'];
-			  $cat = $_GET['cat'];
-			  list($reactiondir, $extension) = explode(".", $blogpost);
-			  
-			  include("data/blog/$cat/reactions/$reactiondir/$file");
-           //Include Translation data
-			  include ("data/settings/langpref.php");
-			  include ("data/inc/lang/en.php");
-			  include ("data/inc/lang/$langpref");
-			  
-			  echo "<div class=\"blogpost\" style=\"margin-bottom: 15px; margin-top: 5px;\">
-			  <span class=\"posttitle\" style=\"font-size: 16px;\">$title</span><br />
-			  <span class=\"postinfo\" style=\"font-size: 10px;\">$lang_blog18 <span style=\"font-weight: bold;\">$name</span> - $postdate</span><br />
-			  $message
-			  </div>";
-			  }
-   }
-   closedir($path);
+	<span class="postinfo" style="font-size: 10px;">
+		<?php echo $lang_blog18; ?> <span style="font-weight: bold;"><?php echo $post_reaction_name[$key]; ?></span> - $postdate
+	</span><br />
+	<?php echo $post_reaction_content[$key]; ?>
+</div>
+	<?php }
 }
-//and then include the reactions
-read_reactions("data/blog/$cat/reactions/$reactiondir/");
+
+//REACTION HTML
+//echo 
 
 //...and show a form to place new reactions
-//echo "<form method=\"post\" action=\"\" style=\"margin-top: 5px; margin-bottom: 15px;\"><div>
-//$lang_blog17 <br /><input name=\"title\" type=\"text\" value=\"\" /><br />
-//$lang_contact3 <br /><input name=\"name\" type=\"text\" value=\"\" /><br />
-//$lang_contact5 <br /><textarea name=\"message\" rows=\"7\" cols=\"45\"></textarea><br />
-//<input type=\"submit\" name=\"Submit\" value=\"$lang_contact10\" />
-//</div></form>";
+?>
 
+	<form method="post" action="" style="margin-top: 5px; margin-bottom: 15px;">
+		<?php echo $lang_blog17; ?> <br /><input name="title" type="text" value="" /><br />
+		<?php echo $lang_contact3; ?> <br /><input name="name" type="text" value="" /><br />
+		<?php echo $lang_contact5; ?> <br /><textarea name="message" rows="7" cols="45"></textarea><br />
+		<input type="submit" name="Submit" value="<?php echo $lang_contact10; ?>" />
+	</form>
+</div>
+
+<?php
 //If form is posted...
 if(isset($_POST['Submit'])) {
-//First fetch our posted variables
-$title = $_POST['title'];
-$name = $_POST['name'];
-$message = $_POST['message'];
 
-//Then check if everything has been filled in
-if (($title) && ($name) && ($message)) {
+	//Check if everything has been filled in
+	if((!isset($_POST['title'])) && (!isset($_POST['name'])) && (!isset($_POST['message']))) { ?>
+		<span style="color: red;"><?php echo $lang_contact6; ?></span>
+	<?php }
 
-//Check for HTML, and eventually block it
-if ((ereg("<", $title)) || (ereg(">", $title)) || (ereg("<", $name)) || (ereg(">", $name)) || (ereg("<", $message)) || (ereg(">", $message))) {
-echo "<span style=\"color: red;\">$lang_blog22</span>"; }
+	else {
+		//Then fetch our posted variables
+		$title = $_POST['title'];
+		$name = $_POST['name'];
+		$message = $_POST['message'];
 
-//If no HTML is present
-else {
+		//Check for HTML, and eventually block it
+		if ((ereg('<', $title)) || (ereg('>', $title)) || (ereg('<', $name)) || (ereg('>', $name)) || (ereg('<', $message)) || (ereg('>', $message))) { ?>
+			<span style="color: red;"><?php echo $lang_blog22; ?></span>
+		<?php }
 
-//Then delete unwanted characters etc.
-$title = stripslashes($title);
-$title = str_replace("\"", "\\\"", $title);
-$name = stripslashes($name);
-$name = str_replace("\"", "\\\"", $name);
-$message = stripslashes($message);
-$message = str_replace("\"", "\\\"", $message);
-$message = str_replace("\n", "<br />", $message);
+		//If no HTML is present
+		else {
+			//Delete unwanted characters
+			$title = stripslashes($title);
+			$title = str_replace("\"", "\\\"", $title);
+			$name = stripslashes($name);
+			$name = str_replace("\"", "\\\"", $name);
+			$message = stripslashes($message);
+			$message = str_replace("\"", "\\\"", $message);
+			$message = str_replace("\n", "<br />", $message);
 
-//Now we have to check which filenames are already in use 
-if (file_exists("data/blog/$cat/reactions/$reactiondir/message1.php")) {
-$i = 2;
-$o = 3;
-while ((file_exists("data/blog/$cat/reactions/$reactiondir/message$i.php")) || (file_exists("data/blog/$cat/reactions/$reactiondir/message$o.php"))) {
-$i = $i+1;
-$o = $o+1; }
-$newfile = "data/blog/$cat/reactions/$reactiondir/message$i.php"; }
-else {
-$newfile = "data/blog/$cat/reactions/$reactiondir/message1.php";
-}
-$data = $newfile;
+			//Determine the date
+			$day = date("d");
+			$month = date("m");
+			$year = date("Y");
+			$time = date("H:i");
 
-//Determine the date
-$day = date("d");
-$month = date("m");
-$year = date("Y");
-$time = date("H:i");
-$date = "$month-$day-$year, $time";
+			//Then, save existing post information
+			$file = fopen('data/settings/modules/blog/posts/'.$post, 'w');
+			fputs($file, '<?php'."\n"
+			.'$post_title = "'.$post_title.'";'."\n"
+			.'$post_category = "'.$post_category.'";'."\n"
+			.'$post_content = "'.$post_content.'";'."\n"
+			.'$post_day = "'.$post_day.'";'."\n"
+			.'$post_month = "'.$post_month.'";'."\n"
+			.'$post_year = "'.$post_year.'";'."\n"
+			.'$post_time = "'.$post_time.'";'."\n");
 
-$file = fopen($data, "w");
-fputs($file, "<?php
-\$title = \"$title\";
-\$name = \"$name\";
-\$message = \"$message\";
-\$postdate = \"$date\";
-?>");
-fclose($file);
+			//Check if there already are other reactions
+			if(isset($post_reaction_title)) {
+				foreach($post_reaction_title as $reaction_key => $value) {
+					//Set key
+					$key = $reaction_key + 1;
+					//And save the existing reaction
+					fputs($file, '$post_reaction_title['.$reaction_key.'] = "'.$post_reaction_title[$reaction_key].'";'."\n"
+					.'$post_reaction_name['.$reaction_key.'] = "'.$post_reaction_name[$reaction_key].'";'."\n"
+					.'$post_reaction_content['.$reaction_key.'] = "'.$post_reaction_content[$reaction_key].'";'."\n"
+					.'$post_reaction_day['.$reaction_key.'] = "'.$post_reaction_day[$reaction_key].'";'."\n"
+					.'$post_reaction_month['.$reaction_key.'] = "'.$post_reaction_month[$reaction_key].'";'."\n"
+					.'$post_reaction_year['.$reaction_key.'] = "'.$post_reaction_year[$reaction_key].'";'."\n"
+					.'$post_reaction_time['.$reaction_key.'] = "'.$post_reaction_time[$reaction_key].'";'."\n");
+				}
+			}		
 
-//Encode items so that blogname can be placed in URL
-$pageback = urlencode($pageback);
-$blogpost = urlencode($blogpost);
-$cat = urlencode($cat);
-echo "<META HTTP-EQUIV=\"REFRESH\" CONTENT=\"0; URL=?blogpost=$blogpost&amp;cat=$cat&amp;pageback=$pageback\">";
+			//If this is the first reaction, use key '0'
+			else {
+				$key = 0;
+			}
+
+			//Then, save reaction
+			fputs($file, '$post_reaction_title['.$key.'] = "'.$title.'";'."\n"
+			.'$post_reaction_name['.$key.'] = "'.$name.'";'."\n"
+			.'$post_reaction_content['.$key.'] = "'.$message.'";'."\n"
+			.'$post_reaction_day['.$key.'] = "'.$day.'";'."\n"
+			.'$post_reaction_month['.$key.'] = "'.$month.'";'."\n"
+			.'$post_reaction_year['.$key.'] = "'.$year.'";'."\n"
+			.'$post_reaction_time['.$key.'] = "'.$time.'";'."\n"
+			.'?>');
+			fclose($file);
+			chmod('data/settings/modules/blog/posts/'.$post,0777);
+
+			//Redirect user
+			redirect('?module=blog&page=viewpost&post='.$post.'&pageback='.$pageback,'0');
+		}
 	}
 }
-
-//...or if not all fields are filled
-else {
-echo "<span style=\"color: red;\">$lang_contact6</span>"; }
-}
-
-//Show a link to go back
-echo "<p><a href=\"?file=$pageback\">&lt;&lt;&lt; $lang_theme12</a></p>";
 ?>
+
+<p><a href="?file=<?php echo $pageback; ?>">&lt;&lt;&lt; <?php echo $lang_theme12; ?></a></p>
