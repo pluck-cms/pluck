9e517a5d4f8a9cc1a39bf89a746080c04d2d6b17
Anders Jørgensen <spirit55555@gmail.com>
2008-10-09 21:55:29 +0200

    Theme XHTML cleanup

--- data/inc/theme.php
+++ data/inc/theme.php
@@ -2,7 +2,8 @@
 /*
  * This file is part of pluck, the easy content management system
  * Copyright (c) somp (www.somp.nl)
- * http://www.pluck-cms.org
+ * http://www.pluck-cms.org
+
  * Pluck is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -19,59 +20,60 @@ if((!ereg("index.php", $_SERVER['SCRIPT_FILENAME'])) && (!ereg("admin.php", $_SE
     exit();
 }
 ?>
-
 <div class="rightmenu">
-<div style="background-color: #f4f4f4; padding: 5px; width: 300px; margin-top: 15px; border: 1px dotted gray;">
-	<table>
-		<tr>
-			<td>
-				<img src="data/image/install.png" border="0" alt="">
-			</td>
-			<td>
-			<?php
-				// if zlib is installed
-				if (get_extension_funcs('zlib')) {
-					echo '<span style="font-size: 17pt;"><a href="?action=themeinstall">'.$lang_theme5.'</a></span>';
-				}
-				// if zlib is not installed
-				elseif (!get_extension_funcs('zlib')) {
-					echo '<span class="kop3">'.$lang_theme5.'</span><br>';
-					echo '<span style="color: red;">'.$lang_theme14.'</span>';
-				}
-			?></td>
-		</tr>
-	</table>
+	<div class="menudiv" style="padding-right: 120px;">
+		<table>
+			<tr>
+				<td>
+					<img src="data/image/install.png" alt="" />
+				</td>
+				<td>
+				<?php
+					//If zlib is installed
+					if (get_extension_funcs('zlib')) {
+						echo '<span class="kop3"><a href="?action=themeinstall">'.$lang_theme5.'</a></span>';
+					}
+					//If zlib is not installed
+					elseif (!get_extension_funcs('zlib')) {
+						echo '<span class="kop3">'.$lang_theme5.'</span><br />';
+						echo '<span class="red">'.$lang_theme14.'</span>';
+					}
+				?></td>
+			</tr>
+		</table>
+	</div>
 </div>
-</div>
-<p><b><?php echo $lang_theme1; ?></b></p>
+
+<p><strong><?php echo $lang_theme1; ?></strong></p>
 
 <form action="" method="post">
 	<select name="cont">
-		<option value="0"><?php echo $lang_lang2; ?>
-
-<?php
-$dirs = read_dir_contents('data/themes','dirs');
-if($dirs) {
-	natcasesort($dirs);
-	foreach ($dirs as $dir) {
-		if (file_exists('data/themes/'.$dir.'/info.php')) {
-			include ('data/themes/'.$dir.'/info.php');
-			//If theme is current theme, select it
-			if ($themedir == $site_theme) { ?>
-				<option value="<?php echo $themedir; ?>" selected><?php echo $themename; ?>
-			<?php }
-			//Otherwise, don't select it
-			else { ?>
-				<option value="<?php echo $themedir; ?>"><?php echo $themename; ?>
-			<?php }
+		<option value="0"><?php echo $lang_lang2; ?></option>
+		<?php
+		$dirs = read_dir_contents('data/themes','dirs');
+		if($dirs) {
+			natcasesort($dirs);
+			foreach ($dirs as $dir) {
+				if (file_exists('data/themes/'.$dir.'/info.php')) {
+					include ('data/themes/'.$dir.'/info.php');
+					//If theme is current theme, select it
+					if ($themedir == $site_theme) { ?>
+						<option value="<?php echo $themedir; ?>" selected="selected"><?php echo $themename; ?></option>
+					<?php }
+					//Otherwise, don't select it
+					else {
+					?>
+						<option value="<?php echo $themedir; ?>"><?php echo $themename; ?></option>
+					<?php
+					}
+				}
+			}
 		}
-	}
-}
-?>
-
-	</select><br />
-	<input type="submit" name="Submit" value="<?php echo $lang_install13; ?>">
-	<input type="button" name="Cancel" value="<?php echo $lang_install14; ?>" onclick="javascript: window.location='?action=options';">
+		?>
+	</select>
+	<br /><br />
+	<input type="submit" name="Submit" value="<?php echo $lang_install13; ?>" />
+	<input type="button" name="Cancel" value="<?php echo $lang_install14; ?>" onclick="javascript: window.location='?action=options';" />
 </form>
 
 <?php
@@ -81,6 +83,6 @@ if((isset($_POST['Submit'])) && ($cont != '0') && (file_exists('data/themes/'.$c
 
 	//Redirect user
 	echo $lang_theme3;
-	redirect('?action=options','2');
+	redirect('?action=options', 2);
 }
 ?>
--- data/inc/themeinstall.php
+++ data/inc/themeinstall.php
@@ -1,53 +1,55 @@
 <?php
-/* 
+/*
  * This file is part of pluck, the easy content management system
  * Copyright (c) somp (www.somp.nl)
- * http://www.pluck-cms.org
+ * http://www.pluck-cms.org
+
  * Pluck is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- 
+
  * See docs/COPYING for the complete license.
 */
 
 //Make sure the file isn't accessed directly
-if((!ereg("index.php", $_SERVER['SCRIPT_FILENAME'])) && (!ereg("admin.php", $_SERVER['SCRIPT_FILENAME'])) && (!ereg("install.php", $_SERVER['SCRIPT_FILENAME'])) && (!ereg("login.php", $_SERVER['SCRIPT_FILENAME']))){
+if((!ereg('index.php', $_SERVER['SCRIPT_FILENAME'])) && (!ereg('admin.php', $_SERVER['SCRIPT_FILENAME'])) && (!ereg('install.php', $_SERVER['SCRIPT_FILENAME'])) && (!ereg('login.php', $_SERVER['SCRIPT_FILENAME']))){
     //Give out an "access denied" error
-    echo "access denied";
+    echo 'access denied';
     //Block all other code
     exit();
 }
 ?>
 
-<p><b><?php echo $lang_theme6; ?></b></p>
+<p><strong><?php echo $lang_theme6; ?></strong></p>
 
 <?php
-if(!isset($_POST['Submit'])) { ?>
-<div style="background-color: #f4f4f4; padding: 5px; width: 500px; margin-top: 15px; border: 1px dotted gray;">
+if(!isset($_POST['Submit'])) {
+?>
+<div class="menudiv">
 	<table>
 		<tr>
 			<td>
-				<img src="data/image/install.png" border="0" alt="" />
+				<img src="data/image/install.png" alt="" />
 			</td>
 			<td>
 				<form method="post" action="" enctype="multipart/form-data">
-					<input type="file" name="sendfile">
-					<input type="submit" name="Submit" value="<?php echo $lang_image9; ?>">
+					<input type="file" name="sendfile" />
+					<input type="submit" name="Submit" value="<?php echo $lang_image9; ?>" />
 				</form>
 			</td>
 		</tr>
 	</table>
 </div>
 
-<div style="background-color: #f4f4f4; padding: 5px; width: 500px; margin-top: 15px; border: 1px dotted gray;">
+<div class="menudiv">
 	<table>
 		<tr>
 			<td>
-				<img src="data/image/themes.png" border="0" alt="" />
+				<img src="data/image/themes.png" alt="" />
 			</td>
 			<td>
-				<span class="kop3"><a href="?action=theme"><<< <?php echo $lang_theme12; ?></a></span>
+				<span class="kop3"><a href="?action=theme">&lt;&lt;&lt; <?php echo $lang_theme12; ?></a></span>
 			</td>
 		</tr>
 	</table>
@@ -56,54 +58,57 @@ if(!isset($_POST['Submit'])) { ?>
 <?php
 }
 
-if(isset($_POST['Submit'])) {	//If no file has been sent
-	if (!$_FILES['sendfile'])  
-		echo $lang_image2; 
-	
+if(isset($_POST['Submit'])) {
+	//If no file has been sent
+	if (!$_FILES['sendfile'])
+		echo $lang_image2;
+
 	else {
 		//Some data
-		$dir = "data/themes"; //where we will save and extract the file 
-		$maxfilesize = "1000000"; //max size of file
+		$dir = 'data/themes'; //where we will save and extract the file
+		$maxfilesize = 1000000; //max size of file
 		$filename = $_FILES['sendfile']['name']; //determine filename
 
 		//Check if we're dealing with a file with tar.gz in filename
-		if(!ereg(".tar.gz", $filename))
+		if(!ereg('.tar.gz', $filename))
 			echo $lang_theme15;
-		else {
 
+		else {
 			//Check if file isn't too big
-			if ($_FILES['sendfile']['size'] > $maxfilesize)  
+			if ($_FILES['sendfile']['size'] > $maxfilesize)
 				echo $lang_theme8;
-			else {  
-				//Save theme-file 
-				copy ($_FILES['sendfile']['tmp_name'], "$dir/$filename")
-				or die ("$lang_image2");
+
+			else {
+				//Save theme-file
+				copy($_FILES['sendfile']['tmp_name'], $dir.'/'.$filename) or die ($lang_image2);
 
 				//Then load the library for extracting the tar.gz-file
-				require("data/inc/lib/tarlib.class.php");
+				require_once('data/inc/lib/tarlib.class.php');
 
 				//Load the tarfile
-				$tar = new TarLib("$dir/$filename");
+				$tar = new TarLib($dir.'/'.$filename);
 
 				//And extract it
 				$tar->Extract(FULL_ARCHIVE, $dir);
 				//After extraction: delete the tar.gz-file
-				unlink("$dir/$filename");
+				unlink($dir.'/'.$filename);
 
 				//Display successmessage
-				echo "<div style=\"background-color: #f4f4f4; padding: 5px; width: 300px; margin-top: 15px; border: 1px dotted gray;\">
-							<table>
-									<tr>
-									<td>
-										<img src=\"data/image/install.png\" border=\"0\" alt=\"\">
-									</td>
-									<td>
-										<span class=\"kop3\">$lang_theme10</span><br />
-										$lang_theme11
-									</td>
-									</tr>
-							</table>
-						</div>";
+				?>
+				<div class="menudiv">
+					<table>
+							<tr>
+								<td>
+									<img src="data/image/install.png" alt="" />
+								</td>
+								<td>
+									<span class="kop3"><?php echo $lang_theme10; ?></span><br />
+									<?php echo $lang_theme11; ?>
+								</td>
+							</tr>
+					</table>
+				</div>
+				<?php		
 			}
 		}
 	}
