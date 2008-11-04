5147b7fe2bab07eac4725ffccd50001188b91f25
Sander Thijsen <sander@ubuntu-nl.org>
2008-11-04 21:30:27 +0100

    Add Turkish language

---
+++ data/inc/lang/tr.php
@@ -0,0 +1,261 @@
+<?php 
+/* 
+This is a pluck language file. You can find pluck at http://www.pluck-cms.org/.
+If you want to help us, please start a new translation on http://www.pluck-cms.org/dev/translate/
+The translation will be included in the next release of pluck.
+
+Note: if you translate, please note the use of capitals: use them sparely.
+
+pluck is licensed under the GNU General Public License, and thus open source.
+This language file is also licensed under the GPL.
+See docs/COPYING for the full license text.
+
+------------------------------------------------
+Language					Turkish
+Translated by			seqizz
+Translators website	http://www.seqizz.net
+------------------------------------------------
+*/
+
+//Name of the language (in English!)
+$lang = "Turkish";
+
+
+//----------------
+//Translation data
+$lang_front1	=	"404: bulunamadı"; 
+$lang_front2	=	"Böyle bir sayfa bulunamadı."; 
+$lang_footer	=	"pluck © 2005-2008 <a href=\"http://www.somp.nl\" target=\"_blank\">somp</a>. pluck, GNU Kamu Lisansı ile dağıtılmaktadır."; 
+$lang_error1	=	"kurulu değil"; 
+$lang_error3	=	"oturum açılmadı"; 
+$lang_error4	=	"Oturum açmış durumda değilsiniz! Lütfen bekleyin... "; 
+$lang_login1	=	"oturum aç"; 
+$lang_login2	=	"pluck henüz kurulmadı. Lütfen bekleyin... "; 
+$lang_login3	=	"şifre"; 
+$lang_login4	=	"oturum aç"; 
+$lang_login5	=	"Şifre doğru. Giriş yapılıyor..."; 
+$lang_login6	=	"Şifre yanlış. Lütfen bekleyin..."; 
+$lang_install	=	"kurulum"; 
+$lang_install1	=	"pluck zaten kurulu. Lütfen bekleyin..."; 
+$lang_install2	=	"Hoşgeldiniz! Yeni sitenizi düzenlemeye başlamadan önce pluck'ı kurmanız gerekiyor."; 
+$lang_install3	=	"Kuruluma başla..."; 
+$lang_install4	=	"adım 1"; 
+$lang_install5	=	"adım 2"; 
+$lang_install6	=	"adım 3"; 
+$lang_install7	=	"'Tazele' butonuna tıklayarak gerekli dosya ve dizinlerin yazılabilirliğini kontrol edin. Eğer hakların doğru verildiğinden eminseniz diğer adıma geçebilirsiniz."; 
+$lang_install8	=	"İyi"; 
+$lang_install9	=	"Hatalı"; 
+$lang_install10	=	"Tazele"; 
+$lang_install11	=	"Uygula..."; 
+$lang_install12	=	"Yeni web siteniz için buraya bir başlık yazın."; 
+$lang_install13	=	"Kaydet"; 
+$lang_install14	=	"İptal"; 
+$lang_install15	=	"Bir başlık girmelisiniz!"; 
+$lang_install16	=	"Bu adımda sitenizin anasayfasını düzenleyebilirsiniz. Bir başlık seçin ve içeriğini düzenleyin."; 
+$lang_install17	=	"başlık"; 
+$lang_install18	=	"içerik"; 
+$lang_install19	=	"pluck başarılı bir şekilde kuruldu"; 
+$lang_install20	=	"websitenize göz atın"; 
+$lang_install21	=	"sonuca göz atın"; 
+$lang_install22	=	"yönetimmerkezi"; 
+$lang_install23	=	"sitenizi yönetin"; 
+$lang_kop1	=	"başlangıç"; 
+$lang_kop2	=	"sayfalar"; 
+$lang_kop4	=	"seçenekler"; 
+$lang_kop5	=	"çıkış"; 
+$lang_kop6	=	"başlığı değiştir"; 
+$lang_kop10	=	"şifreyi değiştir"; 
+$lang_kop11	=	"yeni sayfa"; 
+$lang_kop13	=	"resmi sil"; 
+$lang_kop14	=	"dil ayarları"; 
+$lang_kop15	=	"istatistikler"; 
+$lang_start1	=	"Pluck yönetimmerkezi'ne hoşgeldiniz."; 
+$lang_start3	=	"yardım mı gerek?"; 
+$lang_start4	=	"size yardım etmek isteriz"; 
+$lang_page1	=	"Bu bölümde sayfalarınızı yönetebilir, düzenleyebilir ve silebilirsiniz."; 
+$lang_page2	=	"yeni sayfa"; 
+$lang_page3	=	"sayfayı düzenle"; 
+$lang_page5	=	"Siliniyor..."; 
+$lang_page7	=	"ekle"; 
+$lang_image1	=	"Buradan sitenize koymak için resim yükleyebilirsiniz. Desteklenen üç format var: JPG, PNG ve GIF."; 
+$lang_image2	=	"Yükleme hatalı."; 
+$lang_image3	=	"İsim:"; 
+$lang_image4	=	"Boyut:"; 
+$lang_image5	=	"Tür:"; 
+$lang_image6	=	"Yükleme başarılı!"; 
+$lang_image7	=	"yüklenen resimler"; 
+$lang_image8	=	"resim yükle"; 
+$lang_image9	=	"Başla"; 
+$lang_image11	=	"Bu resim silinemedi."; 
+$lang_image12	=	"Resim silinemiyor. Dosya ve dizinlerin haklarını kontrol edin."; 
+$lang_options1	=	"Burada pluck'ı isteğinize göre düzenleyebilirsiniz."; 
+$lang_options2	=	"sitenizin adını değiştirin"; 
+$lang_options3	=	"sitenizin görünümünü değiştirin"; 
+$lang_options5	=	"şifreyi düzenli aralıklarla değiştirmeniz oldukça iyi bir fikir"; 
+$lang_options8	=	"pluck'ın kullanacağı dili seçin"; 
+$lang_stitle2	=	"Siteniz için bir isim girmelisiniz, boş bırakılamaz."; 
+$lang_siteinfo4	=	"anahtar kelimeler"; 
+$lang_siteinfo5	=	"virgüllerle ayrılmış"; 
+$lang_cpass1	=	"Buradan <i>pluck</i> yönetimmerkezi'ne girmeniz için gereken şifreyi değiştirebilirsiniz. Düzenli aralıklar ile şifreyi değiştirmeniz tavsite edilir."; 
+$lang_cpass2	=	"eski şifre"; 
+$lang_cpass3	=	"yeni şifre"; 
+$lang_cpass4	=	"Şifre değiştirilemedi: girdiğiniz eski şifre yanlış."; 
+$lang_cpass5	=	"Şifre değiştirildi."; 
+$lang_lang1	=	"pluck'ın kullanacağı dili seçin."; 
+$lang_lang2	=	"Seç..."; 
+$lang_lang3	=	"Dil ayarları kaydedildi."; 
+$lang_stats1	=	"Burada sitenizin ziyaretçileri ile ilgili bazı bilgiler bulunmakta. Sayfanın en altında istatistiklerini görmek istediğiniz ay için seçim yapabilirsiniz."; 
+$lang_stats2	=	"istatistik türleri"; 
+$lang_stats3	=	"işletim sistemleri"; 
+$lang_stats4	=	"tarayıcılar"; 
+$lang_stats5	=	"arşivler"; 
+$lang_stats6	=	"Seçeneklerden birini seçin:"; 
+$lang_stats7	=	"toplam"; 
+$lang_stats8	=	"ziyaret"; 
+$lang_stats9	=	"toplam ziyaret"; 
+$lang_stats10	=	"bu ayın ziyaretleri"; 
+$lang_kop16	=	"tema seç"; 
+$lang_theme1	=	"Burada kurduğunuz temalardan birisini seçebilirsiniz."; 
+$lang_theme2	=	"yeni tema bul"; 
+$lang_theme3	=	"Tema ayarları kaydedildi."; 
+$lang_theme4	=	"yeni temaları indirip kurarak sitenizin görünümünü birkaç tıklama ile değiştirebilirsiniz"; 
+$lang_updown1	=	"sayfa sıralamasını değiştir"; 
+$lang_updown2	=	"Bu sayfa zaten en üstte, bu yüzden değiştirilemiyor."; 
+$lang_updown3	=	"Sıra değiştiriliyor..."; 
+$lang_updown4	=	"Bu sayfa zaten en altta, bu yüzden değiştirilemiyor."; 
+$lang_theme5	=	"tema kur"; 
+$lang_theme6	=	"Burada siteniz için yeni temalar kurabilirsiniz. Bunun için temayı indirmiş olmanız gerek."; 
+$lang_theme8	=	"Tema dosyası çok büyük; dosya boyutu en fazla 1MB olabilir."; 
+$lang_theme9	=	"Kurulum başarısız. Muhtemelen sunucuda php-zlib modülü bulunmamakta. Sistem yöneticinizle görüşüp bu modülü kurmasını rica edebilirsiniz."; 
+$lang_theme10	=	"tema kuruldu"; 
+$lang_theme11	=	"<a href=\"?action=theme\">tema sayfası</a>na geri dön"; 
+$lang_theme12	=	"geri"; 
+$lang_install24	=	"e-posta"; 
+$lang_install25	=	"e-posta adresiniz sayesinde ziyaretçileriniz size bir iletişim formu üzerinden ulaşabilecek"; 
+$lang_install26	=	"şifreyi tekrar yazın"; 
+$lang_install27	=	"Lüften kendiniz ve siteniz hakkında genel bazı bilgileri yazın."; 
+$lang_install28	=	"İki farklı şifre girdiniz!"; 
+$lang_install29	=	"adım 4"; 
+$lang_settings	=	"genel ayarlar"; 
+$lang_settings2	=	"buraya siteniz için bir başlık yazın"; 
+$lang_settings3	=	"site başlığı ve e-posta adresiniz gibi bazı genel ayarları değiştirebilirsiniz"; 
+$lang_settings4	=	"Genel ayarlar değiştiriliyor..."; 
+$lang_contact2	=	"diğer seçenekler"; 
+$lang_contact3	=	"İsim:"; 
+$lang_contact4	=	"E-posta:"; 
+$lang_contact5	=	"Mesaj:"; 
+$lang_contact6	=	"Bütün alanları doğru şekilde doldurmadınız."; 
+$lang_contact7	=	"Sitenizdeki formdan mesaj"; 
+$lang_contact8	=	"Mesajınız gönderildi."; 
+$lang_contact9	=	"Mesajınız gönderilemiyor, bir hata oluştu."; 
+$lang_contact10	=	"Gönder"; 
+$lang_kop17	=	"resimleri yönet"; 
+$lang_albums	=	"albümler"; 
+$lang_albums1	=	"Burada albümlerinizi düzenleyebilirsiniz. Ziyaretçilerinize dilediğiniz resim ve fotoğrafları göstermek için albümleri kullanın. İstediğiniz sayfayı düzenlerken \"albüm ekle\"ye tıklayabilirsiniz."; 
+$lang_albums2	=	"albümleri düzenle"; 
+$lang_albums3	=	"yeni albüm"; 
+$lang_albums4	=	"yeni albüm için bir isim yazın ve \"kaydet\"e tıklayın"; 
+$lang_albums5	=	"albümü sil"; 
+$lang_albums6	=	"albümü düzenle"; 
+$lang_albums7	=	"ziyaretçilerinize dilediğiniz resim ve fotoğrafları göstermek için albümleri kullanın"; 
+$lang_albums8	=	"Bu sayfada albümdeki resimleri düzenleyebilir, silebilir ve yenilerini ekleyebilirsiniz. Sadece JPG formatı desteklenmektedir."; 
+$lang_albums9	=	"resimleri düzenle"; 
+$lang_albums10	=	"yeni resim"; 
+$lang_albums11	=	"tanım"; 
+$lang_albums12	=	"kalite (1-100)"; 
+$lang_albums13	=	"buradan yeni bir resim yükleyebilirsiniz. bir başlık ve tanım yazın, ardından resmin işlenmesi sırasında kullanılacak kalite değerini girin. yüksek kalite, büyük dosya boyutu demektir."; 
+$lang_albums14	=	"henüz boş..."; 
+$lang_albums15	=	"resmi düzenle"; 
+$lang_albums16	=	"Sunucunuzda albüm özelliği desteklenmiyor. Muhtemelen php-gd modülü kurulu değil. Sistem yöneticinizle görüşüp bu modülü kurmasını rica edebilirsiniz."; 
+$lang_albums17	=	"şu sayfaya albüm ekle:"; 
+$lang_modules	=	"modüller"; 
+$lang_modules1	=	"Pluck modüler bir yapıdadır. Sitenizi geliştirmek için yeni modüller ekleyebilirsiniz."; 
+$lang_updown5	=	"resim sırasını değiştir"; 
+$lang_updown6	=	"Bu resim zaten en üstte, bu yüzden değiştirilemiyor."; 
+$lang_updown7	=	"Bu resim zaten en altta, bu yüzden değiştirilemiyor."; 
+$lang_pagehide1	=	"sayfayı menüde göster"; 
+$lang_page8	=	"Şu nesneler bu sayfada etkinleştirilmek için hazır:"; 
+$lang_page9	=	"link ekle"; 
+$lang_meta1	=	"sayfa bilgisini düzenle"; 
+$lang_meta2	=	"Bu sayfa ile ilgili bazı bilgiler girebilir, aramalarda daha kolay bulunmasını sağlayabilirsiniz."; 
+$lang_meta4	=	"Sayfa bilgileri değiştiriliyor..."; 
+$lang_settings5	=	"Buradan, site başlığı ve e-posta adresiniz gibi bazı genel ayarları değiştirebilirsiniz."; 
+$lang_blog	=	"günlük"; 
+$lang_blog1	=	"ziyaretçilerinizi bilgilendirmek ve yeni yazılar ulaştırmak için günlük girdilerini kullanın"; 
+$lang_blog2	=	"Bu sayfada, yazılarınızı dahil edebileceğiniz yeni kategoriler oluşturabilirsiniz. Dilediğiniz sayfayı düzenlerken bir kategori ekleyebilirsiniz."; 
+$lang_blog3	=	"kategoriler"; 
+$lang_blog4	=	"yeni kategori"; 
+$lang_blog5	=	"yeni kategori için bir isim yazıp, \"kaydet\"e tıklayın"; 
+$lang_blog6	=	"kategoriyi sil"; 
+$lang_blog7	=	"kategoriyi düzenle"; 
+$lang_blog8	=	"Buradan kategoriye yeni yazılar ekleyebilirsiniz. Yazılar tarihe göre sıralanacaktır."; 
+$lang_blog9	=	"önceki yazılar"; 
+$lang_blog10	=	"yeni yazı yaz"; 
+$lang_blog11	=	"yazıyı düzenle"; 
+$lang_blog12	=	"yazıyı sil"; 
+$lang_blog13	=	"bu sayfaya günlük kategorisi ekle:"; 
+$lang_blog14	=	"yazı kategorisi"; 
+$lang_blog16	=	"yorumlar"; 
+$lang_blog17	=	"Başlık:"; 
+$lang_blog18	=	"yazar"; 
+$lang_blog19	=	"yorumları düzenle"; 
+$lang_blog20	=	"Buradan ziyaretçilerinizin yazınız hakkındaki yorumlarını düzenleyebilirsiniz."; 
+$lang_blog21	=	"yorumu sil"; 
+$lang_albums18	=	"Belirtilen albüm yok."; 
+$lang_blog22	=	"HTML koduna izin verilmiyor."; 
+$lang_trash	=	"çöp"; 
+$lang_trash1	=	"nesneyi çöpe taşı"; 
+$lang_trash2	=	"Nesne çöpe taşınıyor..."; 
+$lang_trash3	=	"çöpteki nesneler"; 
+$lang_trash4	=	"Nesne çöpe taşınamadı: çöpte zaten aynı isimde başka bir nesne var."; 
+$lang_trash5	=	"Silinen nesneler burada listelenmekte. İnceleyebilir, kurtarabilir ya da sonsuza dek silebilirsiniz."; 
+$lang_trash6	=	"çöpü boşalt"; 
+$lang_trash7	=	"nesneye bak"; 
+$lang_trash8	=	"nesneyi çöpten sil"; 
+$lang_trash9	=	"resimler"; 
+$lang_trash10	=	"nesneyi kurtar"; 
+$lang_blog23	=	"yorum ekle ya da incele"; 
+$lang_credits	=	"yapımcılar"; 
+$lang_credits1	=	"pluck'ı geliştiren bütün insanlar"; 
+$lang_credits2	=	"Pluck'ın gelişimine yardımcı olduklarından dolayı aşağıdaki insanlara teşekkürler."; 
+$lang_credits3	=	"proje lideri"; 
+$lang_credits4	=	"çeviri"; 
+$lang_credits5	=	"diğerleri"; 
+$lang_start9	=	"Buradan sitenizi yönetebilirsiniz. Ekranınızın en üstündeki menüden bir link seçin."; 
+$lang_start10	=	"daha fazla..."; 
+$lang_update1	=	"pluck şu anda güncel"; 
+$lang_update2	=	"güncelleme var"; 
+$lang_update3	=	"<b>kritik</b> güncelleme var"; 
+$lang_settings6	=	"XHTML Uyumluluk Modu'nu aç (daha yavaş olabilir)"; 
+$lang_modules2	=	"sürüm"; 
+$lang_modules3	=	"modülleri yönet"; 
+$lang_modules4	=	"sitenize eklenecek modülleri yönetin"; 
+$lang_modules5	=	"Burada modüllerinizi yönetebilirsiniz. Kullanılmayanları kaldırabilir ya da yeni modüller için arama yapabilirsiniz. <i>Siteye modül ekle</i> seçeneği ile yeni modüller ekleyebilirsiniz."; 
+$lang_modules6	=	"Gösterme"; 
+$lang_modules7	=	"Modüllerin hangi sırada gösterileceğini seçin."; 
+$lang_modules8	=	"modül bilgisi"; 
+$lang_modules9	=	"Bir modülü sadece tek sayfada görüntülemek de mümkün. Bunun için sayfayı düzenlerken o sayfaya eklenmesini istediğiniz modülleri seçebilirsiniz."; 
+$lang_modules10	=	"modülü kaldır"; 
+$lang_modules11	=	"Bir modül kur..."; 
+$lang_modules12	=	"Yeni modül bul..."; 
+$lang_modules13	=	"Siteye modül ekle..."; 
+$lang_modules14	=	"siteye modül ekleyin"; 
+$lang_modules15	=	"Buradan site modüllerinin hangi bölgelerde gösterileceğini seçebilirsiniz. Bu ayarlar temaya bağlıdır, yeni bir tema seçtiğinizde bunları da baştan ayarlamanız gerekmektedir. Ayrıca burada yaptığınız değişiklikler bütün siteyi etkiler."; 
+$lang_modules16	=	"Bu sayfada görüntülenmesini istediğiniz modülleri ve sırasını seçin."; 
+$lang_modules17	=	"site"; 
+$lang_modules18	=	"sahibi"; 
+$lang_modules19	=	"Bu modülü kaldırmak istediğinize emin misiniz? Modülle ilgili bütün ayarlar da kaybolacak."; 
+$lang_modules20	=	"Buradan yeni modüller kurabilirsiniz. Ancak önce modülü indirmiş olmanız gerek."; 
+$lang_modules21	=	"yeni modülleri indirip kurarak, birkaç tıklama ile pluck'a yepyeni özellikler kazandırabilirsiniz"; 
+$lang_modules22	=	"yeni modül bul"; 
+$lang_modules23	=	"modül kur"; 
+$lang_modules24	=	"Modül dosyası çok büyük; boyut en fazla 2MB olabilir."; 
+$lang_modules25	=	"modül kuruldu"; 
+$lang_modules26	=	"<a href=\"?action=managemodules\">modül sayfası</a>na geri dön"; 
+$lang_theme14	=	"sunucunuzda tema ve modül kurulumu desteklenmemekte, bu işlemi <a href=\"http://www.pluck-cms.org/docs/doku.php/docs:install_nozlib\" target=\"_blank\">elle</a> yapmak zorundasınız"; 
+$lang_theme15	=	"Kurulum başarısız: belirttiğiniz dosya hatalı."; 
+$lang_update4	=	"güncelleme kontrolü başarısız"; 
+$lang_contact12	=	"iletişim formu"; 
+$lang_contact13	=	"iletişim formu ile ziyaretçilerinizin size mesaj göndermesini sağlayabilirsiniz"; 
+$lang_trash11	=	"Çöpü boşaltmak istediğinize emin misiniz? Çöpteki bütün öğeler kaybolacak."; 
+?>
