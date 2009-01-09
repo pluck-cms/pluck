13b902315e4deae790a5c171f77a5a53d6f543b8
Anders Jørgensen <spirit55555@gmail.com>
2009-01-09 16:44:32 +0100

    Default theme cleanup.

--- data/themes/default/info.php
+++ data/themes/default/info.php
@@ -3,8 +3,8 @@
 //Theme Made by: Kristaps, http://www.fyfi.net
 //You can find pluck at http://www.pluck-cms.org
 
-$themedir = "default";
-$themename = "Default";
-$module_space[0] = "main";
-$module_space[1] = "footer";
+$themedir = 'default';
+$themename = 'Default';
+$module_space[0] = 'main';
+$module_space[1] = 'footer';
 ?>
--- data/themes/default/theme.php
+++ data/themes/default/theme.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo LANG; ?>">
 <head>
 <?php theme_meta(); ?>
 </head>
 
 <body>
-
-<div id="container"><div id="hd">
-<h2 title="<?php theme_sitetitle(); ?>"><?php theme_sitetitle(); ?></h2>
-<ul>
-<?php theme_menu('<li><a href="#file">#title</a></li>','<li><a href="#file" class="act">#title</a></li>'); ?>
-</ul>
+<div id="container">
+	<div id="header">
+		<h1 title="<?php theme_sitetitle(); ?>"><?php theme_sitetitle(); ?></h1>
+		<ul>
+			<?php theme_menu('<li><a href="#file">#title</a></li>', '<li><a href="#file" class="act">#title</a></li>'); ?>
+		</ul>
+	</div>
+	<div id="content">
+		<h2 title="<?php theme_pagetitle(); ?>"><?php theme_pagetitle(); ?></h2>
+		<?php theme_content(); ?>
+		<?php theme_module('main'); ?>
+	</div>
+	<div id="footer">
+		<?php theme_module('footer'); ?>
+		<a href="login.php">admin</a> | powered by <a href="http://www.pluck-cms.org">pluck</a>
+	</div>
 </div>
-
-<div id="ct"><h1 title="<?php theme_pagetitle(); ?>"><?php theme_pagetitle(); ?></h1>
-
-<?php theme_content(); ?>
-<?php theme_module("main"); ?>
-
-</div>
-<div id="ft">
-<?php theme_module("footer"); ?>
-<a href="login.php">admin</a> | powered by <a href="http://www.pluck-cms.org">pluck</a>
-</div></div>
 </body>
 </html>
