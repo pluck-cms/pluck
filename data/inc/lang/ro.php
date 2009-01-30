654301a6c9dc87a3b3c9001a53c92812d5eb5d5f
Sander Thijsen <sander@ubuntu-nl.org>
2009-01-30 21:09:48 +0100

    Add Romanian translation by Adi Roiban

---
+++ data/inc/lang/ro.php
@@ -0,0 +1,308 @@
+<?php
+/*
+This is a pluck language file. You can find pluck at http://www.pluck-cms.org/.
+If you want to help us, please start a new translation on http://www.pluck-cms.org/dev/translate/
+The translation will be included in the next release of pluck.
+
+Note: if you translate, please note the use of capitals: use them sparely.
+
+pluck is licensed under the GNU General Public License, and thus open source.
+This language file is also licensed under the GPL.
+See docs/COPYING for the full license text.
+
+------------------------------------------------
+Language		Romanian
+Translators:    Adi Roiban https://launchpad.net/~adiroiban
+------------------------------------------------
+*/
+
+//Name of the language
+$lang = "Romanian";
+
+//----------------
+//Translation data
+
+//Frontend
+$lang_front1 = "404: negăsit.";
+$lang_front2 = "Această pagină nu a putut fi găsită.";
+
+//General data
+$lang_pluck = "Pluck";
+$lang_footer = "pluck &copy; 2005-2008 <a href=\"http://www.somp.nl\" target=\"_blank\">somp</a>. Pluck este disponibil sub termenii licenței GNU General Public License.";
+$lang_error1 = "Nu este instalat";
+$lang_error3 = "Nu sunteți autentificat";
+$lang_error4 = "Nu sunteți autentificat! . Așteptați o clipă...";
+
+//Login page
+$lang_login1 = "Autentificare";
+$lang_login2 = "Pluck nu a fost instalat încă. Așteptați o clipă...";
+$lang_login3 = "Parola";
+$lang_login4 = "Autentifică";
+$lang_login5 = "Parolă corectă. Se autentifică...";
+$lang_login6 = "Parolă incorectă. Așteptați o clipă...";
+
+//Install page
+$lang_install = "Instalarea";
+$lang_install1 = "Pluck a fost instalat deja. Așteptați o clipă...";
+$lang_install2 = "Bun venit! Înainte de a configura siteul, trebui să instalați Pluck.";
+$lang_install3 = "Începe instalarea...";
+$lang_install4 = "Pasul 1";
+$lang_install5 = "Pasul 2";
+$lang_install6 = "Pasul 3";
+$lang_install7 = "Verificați că fișierele afișate pot fi scrise. Puteți actualiza pagina, apăsând pe butonul „Reîncarcă”. În cazul în care sunteți siguri că fișierele pot fi scrise, puteți trece la următorul pas.";
+$lang_install8 = "Corect";
+$lang_install9 = "Greșit";
+$lang_install10 = "Reîncarcă";
+$lang_install11 = "Continuă...";
+$lang_install12 = "Alegeți numele siteului.";
+$lang_install13 = "Salvează";
+$lang_install14 = "Anulează";
+$lang_install15 = "Trebuie să introduceți un titlu!";
+$lang_install16 = "De aici puteți modifica pagina principală a siteului. Alegeți un titlu și adăugați conținut.";
+$lang_install17 = "Titlu";
+$lang_install18 = "Conținut";
+$lang_install19 = "Pluck a fost instalat cu succes!";
+$lang_install20 = "Vizualizați siteul";
+$lang_install21 = "Aruncați o privire peste rezultat";
+$lang_install22 = "Centrul de administrare";
+$lang_install23 = "Administrați siteul dumneavoastră";
+
+//Admincenter:Titles
+$lang_kop1 = "Start";
+$lang_kop2 = "Pagini";
+$lang_kop4 = "Opțiuni";
+$lang_kop5 = "Ieșire";
+$lang_kop6 = "Modificare titlu";
+$lang_kop10 = "Schimbare parolă";
+$lang_kop11 = "Pagină nouă";
+$lang_kop13 = "Șterge imaginea";
+$lang_kop14 = "Configurări limbă";
+
+//Admincenter:: Start
+$lang_start1 = "Bun venit la pagina de administrare Pluck!";
+$lang_start3 = "Aveți nevoie de ajutor?";
+$lang_start4 = "Suntem bucuroși să vă putem ajuta.";
+
+//Admincenter:: Pages
+$lang_page1 = "De aici puteți administra, șterge sau creea pagini.";
+$lang_page2 = "Pagină nouă";
+$lang_page3 = "Modificare pagină";
+//Admincenter:: Pages:: Deletepage
+$lang_page5 = "Se șterge...";
+//Admincenter:: Pages:: Rightmenu
+$lang_page7 = "Introdu";
+
+//Admincenter:: Images
+$lang_image1 = "De aici puteți încărca imagini ce pot fi introduce mai târziu în paginile dumneavoastră. Sunt suportate trei tipuri de imagini: JPG, PNG și GIF.";
+$lang_image2 = "Încărcarea a eșuat.";
+$lang_image3 = "Nume:";
+$lang_image4 = "Mărime:";
+$lang_image5 = "Tip:";
+$lang_image6 = "Încărcare reușită!";
+$lang_image7 = "Imagini încărcate";
+$lang_image8 = "Încărcare imagine";
+$lang_image9 = "Start";
+$lang_image11 = "Această imagine nu poate fi ștearsă.";
+$lang_image12 = "Nu s-a putut șterge această imagine. Verificați permisiunile dosarului și a fișierului.";
+
+//Admincenter:: Options
+$lang_options1 = "De aici puteți configura Pluck pentru a corespunde dorințelor dumneavoastră.";
+$lang_options2 = "De aici puteți schimba numele siteului";
+$lang_options3 = "Schimbați aspectul și modul de afișare al siteului";
+$lang_options5 = "Se recomandă schimbarea periodică a parolei";
+$lang_options8 = "Alegeți limba folosită de Pluck";
+
+//Admincenter:: Options:: Sitetitle
+$lang_stitle2 = "Trebuie să introduceți un nume pentru site. Acest câmp nu poate fi lăsat necompletat.";
+
+//Admincenter:: Options:: Siteinfo
+$lang_siteinfo4 = "Cuvinte cheie";
+$lang_siteinfo5 = "despărțite de virgulă";
+
+//Admincenter:: Options:: Changepass
+$lang_cpass1 = "De aici puteți schimba parola folosită pentru autentificarea pe pagina de administrare <i>Pluck</i>. Se recomandă schimbarea perioadică a parolei.";
+$lang_cpass2 = "Parola veche";
+$lang_cpass3 = "Parola nouă";
+$lang_cpass4 = "Nu s-a putut schimba parola: vechea parolă introdusă nu este corectă.";
+$lang_cpass5 = "Parola a fost schimbată.";
+
+//Admincenter:: Options:: Languagesettings
+$lang_lang1 = "Alegeți limba folosită în Pluck.";
+$lang_lang2 = "Alegeți...";
+$lang_lang3 = "Configurările limbii au fost salvate.";
+
+//Admincenter:: Stats
+//FIXME: Do we need these anymore?
+$lang_stats1 = "De aici puteți vizualiza unele informații despre vizitatorii siteului dumneavoastră. În josul pagini veți putea alege luna pentru care să fie afișate statisticile.";
+$lang_stats2 = "Statistici de la";
+$lang_stats3 = "Sisteme de operare";
+$lang_stats4 = "Navigatoare";
+$lang_stats5 = "Arhive";
+$lang_stats6 = "Alegeți unul dintre următoarele elemente:";
+$lang_stats7 = "Total";
+$lang_stats8 = "Accesări";
+$lang_stats9 = "Accesări totale";
+$lang_stats10 = "Accesări în această lună";
+
+//VERSION 4.2 NEW
+//---------------
+$lang_kop16 = "Alegere aspect";
+$lang_theme1 = "De aici puteți alege care dintre temele instalate să fie folosite.";
+$lang_theme3 = "Configurările de aspect au fost salvate.";
+$lang_updown1 = "Schimbă ordinea pagini";
+$lang_updown2 = "This page already is on the top, so its rank can't be changed.";
+$lang_updown3 = "Changing rank...";
+$lang_updown4 = "This page already is the last one, so its rank can't be changed.";
+
+//VERSION 4.3 NEW
+//---------------
+$lang_theme5	=	"Instalare temă";
+$lang_theme6	=	"De aici puteți instala o nouă temă. Inainte de a continua asigurați-vă că ați descărcat un fișier cu tema.";
+$lang_theme8	=	"Fișierul temă este prea mare; Limata este 1MB.";
+$lang_theme9	=	"Installation failed. The server probably doesn't have installed the php-zlib module. You can contact your systemadministrator and ask to install this module.";
+$lang_theme10	=	"Tema a fost instalată";
+$lang_theme11	=	"Înapoi pe <a href=\"?action=theme\">pagina temei</a>";
+$lang_theme12	=	"Înapoi";
+$lang_install24 = "Email";
+$lang_install25 = "Adresa dumneavoastră de email va fi folosită pentru a permite vizitatorilor să vă contacteze prin intermediul formularului de contact";
+$lang_install26 = "Reintroduceți parola";
+$lang_install27 = "Introduceți câteva informații generale despre dumneavoastră și site.";
+$lang_install28 = "Ați introdus două parole diferite !";
+$lang_install29 = "Pasul 4";
+$lang_settings	= "Configurări generale";
+$lang_settings2 = "Alegeți titlul siteului";
+$lang_settings3 = "Schimbați configurări generale precum titlul siteului sau adresa de email";
+$lang_settings4 = "Se modifică configurările generale...";
+$lang_contact2 = "Alte opțiuni";
+$lang_contact3 = "Nume:";
+$lang_contact4 = "Adresă de email:";
+$lang_contact5 = "Mesaj:";
+$lang_contact6 = "Nu ați introdus toate câmpurile obligatorii.";
+$lang_contact7 = "Mesaj de pe forumarul de contact";
+$lang_contact8 = "Mesajul dumneavoastră a fost trimis cu succes.";
+$lang_contact9 = "S-a produs o eroare iar mesajul dumneavoastră nu a putut fi trimis.";
+$lang_contact10 = "Trimite";
+$lang_kop17 = "Administrare imagini";
+$lang_albums = "Albume";
+$lang_albums1 = "Here you can manage your albums. Use albums to show your visitors your favourite photos and images. Insert the albums in your page(s) by choosing \"insert album\" when editing a page.";
+$lang_albums2 = "Modificare albume";
+$lang_albums3 = "Album nou";
+$lang_albums4 = "Alegeți un nume pentru noul album, apoi faceți clic pe  „Salvează”";
+$lang_albums5 = "Ștergere album";
+$lang_albums6 = "Modificare album";
+$lang_albums7 = "Folosiți albumele pentru a afișa fotografiile și imaginile dumneavoastră";
+$lang_albums8 = "Folosiți această pagină pentru a adăuga, a șterge sau a modifica imagini din albumul dumneavoastră. Sunt suportate doar imagini <b>JPG</b>.";
+$lang_albums9 = "Modificare imagini";
+$lang_albums10 = "Imagine nouă";
+$lang_albums11 = "Descriere";
+$lang_albums12 = "Calitate (1-100)";
+$lang_albums13 = "De aici puteți încărca o nouă imagine. Alegeți un titlu și o descriere precum și calitatea la care imaginea va fi salvată. O calitate mai mare va determina o dimensiune mai mare.";
+$lang_albums14 = "Încă nimic...";
+$lang_albums15 = "Modificare imagine";
+$lang_albums16	= "Albumele nu sunt suportate pe acest server: foarte probabil din cauza faptului că modulul php-gd nu este instalat. Puteți contacta administratorul de sistem pentru a-i cere instalarea modulului.";
+$lang_albums17	= "Include albumul în această pagină:";
+
+//VERSION 4.4 NEW
+//---------------
+$lang_modules = "Module";
+$lang_modules1 = "Pluck dispune de o varietate de module pe care le puteți folosi pentru a extinde siteul introducând conținut dinamic.";
+$lang_updown5 = "Schimbă ordinea imagini";
+$lang_updown6 = "This image already is on the top, so its rank can't be changed.";
+$lang_updown7 = "This image already is the last one, so its rank can't be changed.";
+$lang_pagehide1 = "Afișează pagina în meniu";
+$lang_page8 = "Aceste elemente sunt pregătite pentru a fi introduse în pagină:";
+$lang_page9 = "Introdu legătura";
+$lang_meta1 = "Modifcă informațiile paginii";
+$lang_meta2 = "De aici puteți adugă informații despre pagină, pentru a obține rezultate mai bune în motoarele de căutare.";
+$lang_meta4 = "Se modifică informațiile paginii...";
+$lang_settings5 = "De aici puteți modifica configurări generale precum titlul siteului sau adresa email de contact.";
+$lang_blog = "blog";
+$lang_blog1 = "use a blog to post news or write articles for your visitors";
+$lang_blog3 = "Categorii";
+$lang_blog4 = "Categorie nouă";
+$lang_blog5 = "Alegeți un nume pentru noua categorie, apoi faceți clic pe „Salveză”";
+$lang_blog6 = "Ștergere categorie";
+$lang_blog7 = "Modificare categorie";
+$lang_blog9 = "Articole existente";
+$lang_blog10 = "Scrie un nou articol";
+$lang_blog11 = "Modificare articol";
+$lang_blog12 = "Șterge articol";
+$lang_blog14 = "Adăuga în";
+$lang_blog16 = "Reacții";
+$lang_blog17 = "Titlu:";
+$lang_blog19 = "Modificare reacții";
+$lang_blog20 = "De aici puteți modifica reacțiile vizitatorilor la articolele dumneavoastră.";
+
+//VERSION 4.5 NEW
+//---------------
+$lang_blog21 = "Șterge reacția";
+$lang_albums18 = "Albumul specificat nu există.";
+$lang_blog22 = "Nu este permis cod HTML.";
+$lang_trash = "Coș de gunoi";
+$lang_trash1 = "Mută elementul la gunoi";
+$lang_trash2 = "Se mută elementul la gunoi...";
+$lang_trash3 = "elemente în coșul de gunoi";
+$lang_trash4 = "Elementul nu a putut fi mutat la gunoi: coșul de gunoi deja un nume cu același nume.";
+$lang_trash5 = "Aici sunt afișate elementele șterse. Puteți să le vizualizați, să le recuperați sau să le ștergeți din coșul de gunoi.";
+$lang_trash6 = "Golește coșul de gunoi";
+$lang_trash7 = "Vizualizare element";
+$lang_trash8 = "Ștergere element din coșul de gunoi";
+$lang_trash9 = "Imagini";
+$lang_trash10 = "Recuperare element";
+$lang_blog23 = "Vizualizează sau adaugă reacții";
+$lang_credits = "Autori și mulțumiri";
+$lang_credits1 = "Toate persoanele ce au ajutat la realizarea Pluck";
+$lang_credits2 = "Mulțumirile noastre se adresează următoarelor persoane ce au ajutat la dezvoltarea sistemului Pluck.";
+$lang_credits3 = "Conducător proiect";
+$lang_credits4 = "Traduceri";
+$lang_credits5 = "Mai multe mulțumiri";
+$lang_start9 = "De aici puteți administra siteul dumneavoastră. Alegeți o legătură din meniul de mai sus.";
+$lang_start10 = "Mai mult...";
+$lang_update1 = "Pluck este actualizat";
+$lang_update2 = "Actualizări disponibile";
+$lang_update3 = "Actualizări disponibile <b>urgents</b>";
+$lang_settings6 = "Activează modul de compatibilitate XHTML (poate fi mai încet)";
+
+//VERSION 4.6 NEW
+//---------------
+$lang_modules2 = "Versiune";
+$lang_modules3 = "Administrare module";
+$lang_modules4 = "Administrați module și activați-le în siteul dumneavoastră";
+$lang_modules5 = "De aici administrați modulele. Ștergeți modulele nefolosite sau căutați module noi pentru a îmbunătății siteul cu funcționalități noi. Deasemenea puteți adăuga module în site alegând <i>Adăugare module în site</i>.";
+$lang_modules6 = "Nu afișa";
+$lang_modules7 = "Algeți ordinea în care să fie afișate modulele.";
+$lang_modules8 = "Informații modul";
+$lang_modules9 = "It's also possible to include a module only in one, seperate page. You can do this by editing the page, and then choosing the module you want to include.";
+$lang_modules10 = "Dezinstalare modul";
+$lang_modules11 = "Instalare modul...";
+$lang_modules13 = "Adăugare module în site...";
+$lang_modules14 = "Adăugare module în site";
+$lang_modules15 = "De aici puteți configura în ce zonă a paginii să fie afișate modulele. Aceste configurări sunt specifice temei: în cazul în care schimbați tema, va trebui să le configurați din nou. Aceste configurări vor fi aplicate tuturor paginilor din site.";
+$lang_modules16 = "Alegeți dacă vreți să afișați module pe această pagină și în ce ordine.";
+$lang_modules17 = "Pagină web";
+$lang_modules18 = "Autor";
+$lang_modules19 = "Sigur doriți să dezinstalați acest modul? Configurările modulului vor fi pierdute.";
+$lang_modules20 =	"De aici puteți instala noi module. Înainte de instalare asigurați-vă vă ați descărcat fișierul modul.";
+$lang_modules23 = "Intalare module";
+$lang_modules24 =	"Fișierul modul este prea mare; Limita este de 2MB.";
+$lang_modules25 =	"Modulul a fost instalat";
+$lang_modules26 =	"Înapoi la <a href=\"?action=managemodules\">pagina modulelor</a>";
+$lang_theme14 = "Instalarea de module și teme nu este posibilă pe acest server. Trebui să instalați <a href=\"http://www.pluck-cms.org/docs/doku.php/docs:install_nozlib\" target=\"_blank\">manual</a>";
+$lang_theme15 = "Instalarea a eșuat: fișierul specificat nu este valid.";
+$lang_update4 = "Verificarea actualizării a eșuat";
+$lang_contact12 = "Formular de contact";
+$lang_contact13 = "Prin intermediul unui formalar de contact veți oferii vizitatorilor posibilitatea de a vă trimite mesaje.";
+$lang_trash11 = "Sigur doriți să goliți coșul de gunoi? Toate elementele din el vor fi pierdute.";
+
+//VERSION 4.7 NEW
+//---------------
+$lang_modules27 = "Acest modul nu este compatibil cu versiunea dumneavoastră de Pluck. Din acest motiv a fost dezactivat.";
+$lang_settings7 = "Adresa de email introdusă nu este validă!";
+$lang_blog24 = "De aici puteți adăug articole pe blogul dumneavoastră. Articolele vor fi sortate automat după dată.";
+$lang_blog25 = "Alege categorie...";
+$lang_blog26 = "Categorie";
+$lang_blog27 = "Fără categorie";
+$lang_credits6 = "Dezvoltatori principali";
+$lang_credits7 = "Contribuitori";
+$lang_album19 = 'Există deja un album cu acest nume.'
+?>
