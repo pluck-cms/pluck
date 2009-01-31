6cf15d70af9c221ca53c01339519fc1d10432a13
Sander Thijsen <sander@ubuntu-nl.org>
2009-01-31 12:12:08 +0100

    Add Japanese translation

---
+++ data/inc/lang/ja.php
@@ -0,0 +1,222 @@
+<?php 
+/*
+This is a pluck language file. You can find pluck at http://www.pluck-cms.org/.
+If you want to help us, please start a new translation on http://www.pluck-cms.org/dev/translate/
+The translation will be included in the next release of pluck.
+
+Note: if you translate, please note the use of capitals: use them sparely.
+
+pluck is licensed under the GNU General Public License, and thus open source.
+This language file is also licensed under the GPL.
+See docs/COPYING for the full license text.
+
+------------------------------------------------
+Language		Japanese
+Translated by		Shi-no
+Translators website	none
+------------------------------------------------
+*/
+
+//Name of the language (in English!)
+$lang = "Japanese";
+
+
+//----------------
+//Translation data
+$lang_front2	=	"そのページは見つかりませんでした"; 
+$lang_footer	=	"pluck © 2005-2008 <a href=\"http://www.somp.nl\" target=\"_blank\">somp</a>. pluck is available under the terms of the GNU General Public License."; 
+$lang_error1	=	"インストールされていません"; 
+$lang_error3	=	"ログインしていません"; 
+$lang_error4	=	"ログインしていません! しばらくお待ちください... "; 
+$lang_login1	=	"ログイン"; 
+$lang_login2	=	"plucはまだインストールされていません。しばらくお待ちください..."; 
+$lang_login3	=	"パスワード"; 
+$lang_login4	=	"ログイン"; 
+$lang_login5	=	"正しいパスワードです。ログインしています..."; 
+$lang_login6	=	"パスワードが間違っています。しばらくお待ちください..."; 
+$lang_install	=	"インストール"; 
+$lang_install1	=	"pluckはインストールされました。 しばらくお待ちください..."; 
+$lang_install2	=	"ようこそ! 新しいウェブサイトを立ち上げる前に、pluckをインストールしなければなりません。"; 
+$lang_install3	=	"インストール開始..."; 
+$lang_install4	=	"ステップ 1"; 
+$lang_install5	=	"ステップ 2"; 
+$lang_install6	=	"ステップ 3"; 
+$lang_install7	=	"'Refresh'というボタンをクリックして表示されているファイルまたはディレクトリが書き込み可能になっているか確認する。 それらのファイルが書き込み可能な場合, 次のステップに進んでください."; 
+$lang_install12	=	"新しいウェブサイトのタイトルをここに記入してください"; 
+$lang_install13	=	"保存"; 
+$lang_install14	=	"キャンセル"; 
+$lang_install15	=	"タイトルを入れてください!"; 
+$lang_install16	=	"トップページを編集することができます。 タイトルと内容を記入してください。"; 
+$lang_install17	=	"タイトル"; 
+$lang_install18	=	"内容"; 
+$lang_install19	=	"pluckはインストールされました!"; 
+$lang_install20	=	"ウェブサイトを見る"; 
+$lang_install21	=	"結果を見る"; 
+$lang_install22	=	"管理ページ"; 
+$lang_install23	=	"サイトを管理する"; 
+$lang_kop1	=	"スタート"; 
+$lang_kop2	=	"ページ"; 
+$lang_kop4	=	"オプション"; 
+$lang_kop5	=	"ログアウト"; 
+$lang_kop6	=	"タイトルを変える"; 
+$lang_kop10	=	"パスワードを変える"; 
+$lang_kop11	=	"新しいページ"; 
+$lang_kop13	=	"ページを削除する"; 
+$lang_kop14	=	"言語設定"; 
+$lang_start1	=	"pluck管理ページへようこそ。"; 
+$lang_start3	=	"サポートします!!"; 
+$lang_start4	=	"喜んでお助けいたします
+"; 
+$lang_page1	=	"ページを管理、編集、削除できます"; 
+$lang_page2	=	"新しいページ"; 
+$lang_page3	=	"ページを編集する"; 
+$lang_page5	=	"削除..."; 
+$lang_page7	=	"挿入"; 
+$lang_image1	=	"画像をアップロードでき、後でウェブページに追加できます。 三つの画像タイプに対応しています: JPG, PNG en GIF."; 
+$lang_image2	=	"アップロード失敗。"; 
+$lang_image3	=	"名前:"; 
+$lang_image4	=	"サイズ:"; 
+$lang_image5	=	"タイプ:"; 
+$lang_image6	=	"アップロード成功!"; 
+$lang_image7	=	"アップロードされた画像"; 
+$lang_image8	=	"画像をアップロード"; 
+$lang_image9	=	"スタート"; 
+$lang_image11	=	"この画像は削除できません。"; 
+$lang_image12	=	"画像が削除できません。ファイルとディレクトリの権限を確認してください"; 
+$lang_options1	=	"あなたの望みや好みに合ったようにpluckを構成できます"; 
+$lang_options2	=	"ウェブサイトの名前を変える"; 
+$lang_options3	=	"ウェブサイトの見た目や感じを変える"; 
+$lang_options5	=	"パスワードを定期的に変えるのはよいアイディアです"; 
+$lang_options8	=	"pluckで使われる言語を選びます"; 
+$lang_stitle2	=	"ウェブサイトの名前を入れなければなりません、空白にはできません。"; 
+$lang_siteinfo4	=	"キーワード"; 
+$lang_siteinfo5	=	"コンマで分ける"; 
+$lang_cpass1	=	" <i>pluck</i> 管理ページにログインするパスワードを変更できます。パスワードを定期的に変えるのはよいアイディアです。"; 
+$lang_cpass2	=	"古いパスワード"; 
+$lang_cpass3	=	"新しいパスワード"; 
+$lang_cpass4	=	"パスワードを変更できません: 入力された古いパスワードが間違っています。"; 
+$lang_cpass5	=	"パスワードが変わりました。"; 
+$lang_lang1	=	"pluckに使うパスワードを選んでください。"; 
+$lang_lang2	=	"選択..."; 
+$lang_lang3	=	"言語が保存されました。"; 
+$lang_stats1	=	"このサイトへの訪問者についての情報を見ることができます。このページの下部で、どの月の統計を見るか選べます"; 
+$lang_stats3	=	"オペレーティングシステム（OS）"; 
+$lang_stats4	=	"ブラウザ"; 
+$lang_stats5	=	"アーカイブ"; 
+$lang_stats6	=	"下の選択肢から一つ選んでください:"; 
+$lang_stats7	=	"合計"; 
+$lang_stats8	=	"閲覧数"; 
+$lang_stats9	=	"閲覧数合計"; 
+$lang_stats10	=	"一ヶ月の閲覧数"; 
+$lang_kop16	=	"テーマを選ぶ"; 
+$lang_theme1	=	"使いたいインストールされたテーマを選ぶことができます"; 
+$lang_theme3	=	"テーマ設定は保存されました."; 
+$lang_updown1	=	"ページの順序を変える"; 
+$lang_updown2	=	"このページはすでにトップページです、なのでこの順序は変えられません。"; 
+$lang_updown3	=	"順序変更中..."; 
+$lang_updown4	=	"このページが最後のページです、なのでこの順序は変えられません。"; 
+$lang_theme5	=	"テーマをインストールする"; 
+$lang_theme6	=	"新しいテーマをインストールできます。なず、テーマをダウンロードしたことを確認してください。"; 
+$lang_theme8	=	"このテーマファイルは大きすぎます; 1MBが限度です。"; 
+$lang_theme9	=	"インストール失敗。 たぶんこのサーバーにはphp-zlib モジュールがインストールされていません。システム管理者に連絡してこのモジュールをインストールするように頼んでください。 "; 
+$lang_theme10	=	"テーマがインストールされました"; 
+$lang_theme11	=	"<a href=\"?action=theme\">テーマページ</a>に戻る"; 
+$lang_theme12	=	"戻る"; 
+$lang_install25	=	"このメールアドレスは、メールフォームを通して訪問者と連絡を取り合うのに使われます"; 
+$lang_install26	=	"パスワードをもう一度お願いします"; 
+$lang_install27	=	"あなたとあなたのサイトの一般的な情報を教えてください"; 
+$lang_install28	=	"二つのパスワードが違います!"; 
+$lang_install29	=	"ステップ 4"; 
+$lang_settings	=	"一般設定"; 
+$lang_settings2	=	"ウェブサイトのタイトルを記入してください"; 
+$lang_settings3	=	"ウェブサイトのタイトルやメールアドレスのような一般設定を変更する"; 
+$lang_settings4	=	"一般設定を変更中..."; 
+$lang_contact3	=	"名前:"; 
+$lang_contact4	=	"メール:"; 
+$lang_contact5	=	"メッセージ:"; 
+$lang_contact6	=	"全ての項目をうめていません."; 
+$lang_contact7	=	"ウェブサイトからのメッセージ"; 
+$lang_contact8	=	"あなたのメッセージの送信に成功しました。"; 
+$lang_contact9	=	"エラーが発生しました。メールの送信に失敗しました。"; 
+$lang_contact10	=	"送信"; 
+$lang_kop17	=	"画像管理"; 
+$lang_albums	=	"アルバム"; 
+$lang_albums1	=	"アルバムを管理できます。アルバムは好きな写真や画像を訪問者に見せるために使います。ページを編集するときに「画像挿入」を選ぶとページに画像を挿入できます。"; 
+$lang_albums2	=	"アルバムを編集する"; 
+$lang_albums3	=	"新しいアルバム"; 
+$lang_albums4	=	"まず、アルバムの名前をいれ、そして「保存」をクリック"; 
+$lang_albums5	=	"アルバムを削除する"; 
+$lang_albums6	=	"アルバムを編集する"; 
+$lang_albums7	=	"アルバムは好きな写真や画像を訪問者に見せるために使います"; 
+$lang_albums8	=	"このページではアルバムの画像の追加、削除、編集ができます。 <b>JPG</b>にのみ対応しています。"; 
+$lang_albums9	=	"画像を編集する"; 
+$lang_albums10	=	"新しい画像"; 
+$lang_albums11	=	"説明"; 
+$lang_albums13	=	"新しい画像をアップロードします。,タイトルと説明を記入しqualityを選択してください。qualityが高いとファイルサイズも大きくなります"; 
+$lang_albums14	=	"まだ何もありません..."; 
+$lang_albums15	=	"画像を編集する"; 
+$lang_albums16	=	"このサーバーはアルバムに対応していません: たぶんphp-gd モジュールがインストールされていません。 システム管理者に連絡してこのモジュールをインストールするように頼んでください。"; 
+$lang_albums17	=	"アルバムをこのページに含める:"; 
+$lang_modules	=	"モジュール"; 
+$lang_modules1	=	"Pluckでは様々なモジュールが利用可能で、ウェブサイトをダイナミックに拡張することができます"; 
+$lang_updown5	=	"画像の順番を変える"; 
+$lang_updown6	=	"この画像はすでにトップです、順番は帰ることができません。"; 
+$lang_updown7	=	"この画像はすでに最後です、順番は帰ることができません。"; 
+$lang_pagehide1	=	"めーニューにページを表示する"; 
+$lang_page9	=	"リンクを挿入する"; 
+$lang_meta1	=	"ページ情報を編集する"; 
+$lang_meta2	=	"検索エンジンでより虚位結果を得るために、このページについて何か情報を記入できます。"; 
+$lang_meta4	=	"ページ情報を変更中..."; 
+$lang_settings5	=	"ウェブサイトのタイトルやメールアドレスのような一般設定を変更。"; 
+$lang_blog	=	"ブログ"; 
+$lang_blog1	=	"訪問者への記事を書くかニュースを投稿するためにブログを使う"; 
+$lang_blog3	=	"カテゴリ"; 
+$lang_blog4	=	"新しいカテゴリ"; 
+$lang_blog5	=	"まずカテゴリの名前を決め,「保存」をクリック"; 
+$lang_blog6	=	"カテゴリーを削除する"; 
+$lang_blog7	=	"カテゴリーを編集する"; 
+$lang_blog9	=	"現在ある記事"; 
+$lang_blog10	=	"新しく記事を書く"; 
+$lang_blog11	=	"記事を編集する"; 
+$lang_blog12	=	"記事を削除する"; 
+$lang_blog16	=	"コメント"; 
+$lang_blog17	=	"タイトル:"; 
+$lang_blog19	=	"コメントを編集する"; 
+$lang_blog20	=	"この記事の訪問者のコメントを編集できます。"; 
+$lang_blog21	=	"コメントを削除する"; 
+$lang_albums18	=	"特定のアルバムは存在していません。"; 
+$lang_blog22	=	"HTML-codeは許可されていません・"; 
+$lang_trash	=	"ゴミ箱"; 
+$lang_trash1	=	"アイテムをゴミ箱に移す"; 
+$lang_trash2	=	"アイテムをゴミ箱に移動中..."; 
+$lang_trash3	=	"個のアイテムがゴミ箱にあります"; 
+$lang_trash4	=	"このアイテムはゴミ箱に移すことはできません: 同じ名前のアイテムがゴミ箱にあります。"; 
+$lang_trash5	=	"削除されたアイテムがリストされています。確認し並び替えゴミ箱から削除することができます。"; 
+$lang_trash6	=	"空のゴミ箱"; 
+$lang_trash7	=	"アイテムを見る"; 
+$lang_trash8	=	"ゴミ箱からアイテムを削除する"; 
+$lang_trash10	=	"アイテムを並び替える"; 
+$lang_blog23	=	"コメントを見る/付ける"; 
+$lang_credits1	=	"pluckの開発を助けてくださった人々"; 
+$lang_credits2	=	"pluckの開発に携わった全ての方に感謝いたします"; 
+$lang_credits3	=	"プロジェクトリーダー"; 
+$lang_credits4	=	"翻訳者"; 
+$lang_credits5	=	"スペシャルサンクス"; 
+$lang_start9	=	"ウェブサイトを管理できます. 上のリンクを選んでください"; 
+$lang_update1	=	"pluckは最新版です"; 
+$lang_update2	=	"アップデート可能"; 
+$lang_update3	=	"<b>緊急</b> アップデート可能"; 
+$lang_modules2	=	"バージョン"; 
+$lang_modules3	=	"モジュールを管理する"; 
+$lang_modules4	=	"モジュールの管理しウェブサイトに含める"; 
+$lang_modules5	=	"モジュールを管理します. 使われていないモジュールを削除したり、新しい機能によってウェブサイトを豊かにするためのモジュールをさがしたりする。<i>Aモジュールを追加</i>を選ぶことでウェブサイトにモジュールを追加できます"; 
+$lang_modules6	=	"表示しない"; 
+$lang_modules7	=	"どのモジュールが表示されるか選びます"; 
+$lang_modules8	=	"モジュール情報"; 
+$lang_modules9	=	"別々のページに一つづつしかモジュールは使えません。 ページを編集し使いたいページを選んでください。"; 
+$lang_modules10	=	"モジュールをアンインストールする"; 
+$lang_modules11	=	"モジュールをインストール..."; 
+$lang_modules13	=	"モジュールを追加..."; 
+$lang_modules14	=	"ウェブサイトにモジュールを追加する"; 
+$lang_modules15	=	"どこにモジュールが表示されるか決めてください。なお、この設定は限定的で、もし別のテーマに変更したら、もう一度設定しなおさなければいけません. Please also note that these settings will apply for all pages on your website."; 
+?>
