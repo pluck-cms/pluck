6542b158c7c0048f5380c08e130d4a01f24c3d3e
Anders Jørgensen <spirit55555@gmail.com>
2010-04-02 22:44:47 +0200

    Changed the seo_url() function to work with multibyte languages.

--- data/inc/functions.all.php
+++ data/inc/functions.all.php
@@ -197,17 +197,14 @@ function show_error($message, $level, $return = false) {
  * @return string A SEO safe URL.
  */
 function seo_url($url) {
-	require ('data/inc/lib/url_replace.php');
+	//Decode HTML entities
+	//Replace non-alphanumeric characters to dashes. Exceptions: %, _, -
+	//Note that multiple separators are collapsed automatically by the preg_replace.
+	//Convert all characters to lowercase.
+	//Trim spaces on both sides.
+	$slug = rtrim(mb_strtolower(preg_replace('/[^\p{L}\p{N}_]+/u', '-', preg_replace('/\p{Po}/u', '', html_entity_decode($url)))), '-');
 
-	$url = preg_replace('/( |_)+/', '-', $url);
-	foreach ($lang_url_replace as $old => $new)
-		$url = str_replace($old, $new, $url);
-	$url = urlencode($url);
-	$url = preg_replace('/(%..|\.)/', '', $url);
-	$url = preg_replace('/(-)+/', '-', $url);
-	$url = trim($url, '-');
-	$url = strtolower($url);
-	return $url;
+	return $slug;
 }
 
 /**
