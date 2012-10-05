f9fad72513c54e5b6b9f34419d5150b3c6e01b96
Uranium235 <bzr.gen@voegler.com>
2012-10-05 16:12:00 +0200

    Fix album module incorrectly splitting filename and extension. Bug #962400.

--- data/modules/albums/albums.admin.php
+++ data/modules/albums/albums.admin.php
@@ -110,7 +110,10 @@ function albums_page_admin_editalbum() {
 		//If file is jpeg, pjpeg, png or gif: Accept.
 		if (in_array($_FILES['imagefile']['type'], array('image/jpeg', 'image/pjpeg', 'image/png', 'image/gif'))) {
 			//Define some variables
-			list($image_filename, $ext) = explode('.', $_FILES['imagefile']['name']);
+			$extpos = strrpos($_FILES['imagefile']['name'], '.');
+			if ($extpos === false) $extpos = strlen($_FILES['imagefile']['name']);
+			$image_filename = substr($_FILES['imagefile']['name'], 0, $extpos);
+			$ext = substr($_FILES['imagefile']['name'], $extpos + 1);
 			$image_filename = seo_url($image_filename);
 			$fullimage = ALBUMS_DIR.'/'.$var1.'/'.$image_filename.'.'.strtolower($ext);
 			$thumbimage = ALBUMS_DIR.'/'.$var1.'/thumb/'.$image_filename.'.'.strtolower($ext);
@@ -119,8 +122,10 @@ function albums_page_admin_editalbum() {
 			$images = read_dir_contents(ALBUMS_DIR.'/'.$var1.'/thumb', 'files');
 			if ($images) {
 				foreach ($images as $image) {
-					$parts = explode('.', $image);
-					if ($parts[0] == $image_filename) {
+					$extpos = strrpos($image, '.');
+					if ($extpos === false) $extpos = strlen($image);
+					$namepart = substr($image, 0, $extpos);
+					if ($namepart == $image_filename) {
 						$name_exist = true;
 						break;
 					}
